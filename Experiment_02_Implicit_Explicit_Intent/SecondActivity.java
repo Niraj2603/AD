691bea99def6ba4bfbc70b15fd610eb3858a7622
Niraj Dhage <nirajdhage2603@gmail.com>
2026-04-30 09:01:26 +0530

    Update SecondActivity.java

---
+++ Experiment_02_Implicit_Explicit_Intent/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.intentdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+    Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
+//Go to SecondActivity.java → Clear → Paste:
